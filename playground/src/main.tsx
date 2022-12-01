c4ebfb5562ce3bceac920d2b1950290a15412c9c
snowingfox <1503401882@qq.com>
2022-12-01 22:40:42 +0800

    feat: 插件机制(待完善)

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 const a = 'images'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
